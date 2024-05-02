ec0b748fd551e48745013fb83e0da1271c2313a8
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-02 14:44:48 +0000

    SAFE

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""safe_first_element: Safely retrieve the first element from a sequence"""
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence) -> Union[Any, None]:
+    """Returns the first element of 'lst' or None if 'lst' is empty."""
+    if lst:
+        return lst[0]
+    else:
+        return None
