3ea20e0b2a1b4615178cd2fadafe3bac6b762d69
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-26 12:41:25 +0000

    test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,11 +18,19 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(
-            self, nested_map: dict, path: tuple, expected: any
-            ) -> None:
+    def test_access_nested_map(self, nested_map: dict, path: tuple, expected: any) -> None:
         """Test access_nested_map."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+    ])
+    def test_access_nested_map_exception(self, nested_map: dict, path: tuple, expected_message: str) -> None:
+        """Test access_nested_map exception."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_message}' not found in nested map")
 
 
 if __name__ == "__main__":
