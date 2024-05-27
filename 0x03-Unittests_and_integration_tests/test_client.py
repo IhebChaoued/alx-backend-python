83f497fc789bd5b7364acf81ea94628114c01654
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-27 18:47:40 +0000

    updated

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -41,6 +41,26 @@ class TestGithubOrgClient(unittest.TestCase):
 
         self.assertEqual(result, test_payload["repos_url"])
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """GithubOrgClient.public_repos: returns the correct value"""
+        test_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"}
+        ]
+        mock_get_json.return_value = test_payload
+
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "https://api.github.com/orgs/test/repos"
+
+            test_class = GithubOrgClient("test")
+            result = test_class.public_repos
+
+            mock_get_json.assert_called_once_with("https://api.github.com/orgs/test/repos")
+            mock_public_repos_url.assert_called_once()
+
+            self.assertEqual(result, ["repo1", "repo2"])
+
 
 if __name__ == "__main__":
     unittest.main()
